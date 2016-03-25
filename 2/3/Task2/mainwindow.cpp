8f868f09914017497d5f04c330f41f7b99035829
afletunova <afletunova.yulia@gmail.com>
2016-03-25 21:03:27 +0300

    Added Task3.2

---
+++ 2/3/Task2/mainwindow.cpp
@@ -0,0 +1,55 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QString>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->firstArgument, SIGNAL(valueChanged(int)), this, SLOT(calculator()));
+    connect(ui->secondArgument, SIGNAL(valueChanged(int)), this, SLOT(calculator()));
+    connect(ui->answer, SIGNAL(valueChanged(int)), this, SLOT(calculator()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+/**
+ * @brief MainWindow::calculator after select operation and changing arguments returns result value
+ */
+
+void MainWindow::calculator()
+{
+    int firstArgument = ui->firstArgument->value();
+    int secondArgument = ui->secondArgument->value();
+    char operation = ui->operationBox->currentText()[0].toLatin1();
+
+    switch(operation)
+    {
+    case '+':
+        ui->answer->setText(QString::number(firstArgument + secondArgument));
+        break;
+    case '-':
+        ui->answer->setText(QString::number(firstArgument - secondArgument));
+        break;
+    case '*':
+        ui->answer->setText(QString::number(firstArgument * secondArgument));
+        break;
+    case '/':
+    {
+        if (secondArgument != 0)
+        {
+            ui->answer->setText(QString::number(firstArgument / secondArgument));
+        }
+        else
+        {
+            ui->answer->setText(QString("Error!"));
+        }
+        break;
+    }
+    }
+}
