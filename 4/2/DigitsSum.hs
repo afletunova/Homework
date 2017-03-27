ecf400a0c23769c0350b30760804f3ab2eb4e78d
Julia <afletunova.yulia@gmail.com>
2017-03-27 12:21:13 +0400

    Update DigitsSum.hs

--- 4/2/DigitsSum.hs
+++ 4/2/DigitsSum.hs
@@ -2,7 +2,6 @@ digitsSum :: Integer -> Integer
 
 digitsSum x | x >= 0 = helper x
             | otherwise = helper (abs x)
-
-	where 
-		helper 0 = 0
-		helper x = x `mod` 10 + helper (x `div` 10
+	    where 
+	    	helper 0 = 0
+		helper x = x `mod` 10 + helper (x `div` 10)
