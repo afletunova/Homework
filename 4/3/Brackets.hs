900cb4f3a76cfdaea5c9eab4ffc606568fbc21e4
Afletunova <afletunova.yulia@gmail.com>
2017-04-11 12:10:23 +0300

    Add another solution of the brackets validation problem

--- 4/3/Brackets.hs
+++ 4/3/Brackets.hs
@@ -1,22 +1,15 @@
 validation :: String -> Bool
 
-validation xs = check 0 0 0 [] xs == 0
-	where 
-		check n m k ls [] = abs(n) + abs(m) + abs(k)
-		check n m k ls (x:xs)
-			| n < 0     = n
-			| m < 0     = m
-			| k < 0     = k
-			| x == ')' && (ls == ['{'] || ls == ['[']) 
-						= m + k
-			| x == '}' && (ls == ['('] || ls == ['[']) 
-						= n + k
-			| x == ']' && (ls == ['('] || ls == ['{']) 
-						= n + m
-			| x == '('  = check (n + 1) m k ['('] xs
-			| x == ')'  = check (n - 1) m k [')'] xs
-			| x == '['  = check n (m + 1) k ['['] xs
-			| x == ']'  = check n (m - 1) k [']'] xs
-			| x == '{'  = check n m (k + 1) ['{'] xs
-			| x == '}'  = check n m (k - 1) ['}'] xs
-			| otherwise = check n m k ls xs  
+validation xs = check xs []
+	where
+		check [] s = (length s == 0)
+		check ('(':xs) s = check xs ('(':s)
+		check ('{':xs) s = check xs ('{':s)
+		check ('[':xs) s = check xs ('[':s)
+		check (')':xs) ('(':s) = check xs s
+		check ('}':xs) ('{':s) = check xs s
+		check (']':xs) ('[':s) = check xs s
+		check (')':xs) s = False
+		check ('}':xs) s = False
+		check (']':xs) s = False
+		check (x:xs) s = check xs s
